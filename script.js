02ce49be09f645dd86115dc0ca521f1134714cbb
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-28 22:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //your JS code here. If required.
-//your JS code here. If required.
-function removecolor()
-{
-var x=document.getElementById("colorSelect");
-x.remove(x.selectedIndex);
+function remove(){
+	var element=document.getElementById("colorSelect")
+	element.remove(element.selectedIndex)
 }
